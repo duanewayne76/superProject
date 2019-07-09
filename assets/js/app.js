3f584fbef0b5c497b1d879ba40563fcb8778d5c7
Cohen <dcoco91@gmail.com>
2019-07-09 22:48:38 +0300

    added the JSON to the link

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
             $("#user-link").val("");
             $('#answer').removeClass("d-none");
 
-            var ipURL = "https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_3fGj57nrEWvCnPmWisKfWJwaVgE7N&domainName=google.com";
+            var ipURL = "https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_3fGj57nrEWvCnPmWisKfWJwaVgE7N&domainName=google.com&outputFormat=JSON";
 
             $.ajax({
                 url: ipURL,
