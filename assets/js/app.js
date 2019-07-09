21caab75017368c059388a618613a75992b82195
Cohen <dcoco91@gmail.com>
2019-07-09 23:23:42 +0300

    added a working API call that brings back the IP address of the given website

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,51 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    var x = $('div');
-    var isValid = false;
+    function getUrlInfo(url) {
+
+        let url2Check = url;
+        let aKey = "at_3fGj57nrEWvCnPmWisKfWJwaVgE7N";
+        let ipURL = `https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=${aKey}&domainName=${url2Check}&outputFormat=JSON&ip=1`;
+        
+
+        $.ajax({
+            url: ipURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            var ip = response.WhoisRecord.ips[0];
+            // console.log(ip);
+            var x = $('<div>');
+            x.html(`<div class="p-2"><h2>IP: ${ip}</h2></div>`);
+            x.attr("id", "txt");        
+            $('#stuff').append(x);
+
+        });
+    }
 
 
-
-    $("#submit-button").on("click", function(event) {
+    $("#submit-button").on("click", function (event) {
         $("#img").empty();
         $("#stuff").empty();
         event.preventDefault();
 
 
-        var ak = "c6a18f9be9e8692a7c9ef136e473eb41";
+
         var url = $('#user-link').val().trim();
         if (url) {
 
+            // function that takes the url the user gave and performs an AJAX call for Info
+            getUrlInfo(url);
 
+            $("#user-link").val("");
+            $('#answer').removeClass("d-none");
+
+            // img created from thum.io and appended onto the page
             var auth = '4775-012efdf683fa5f53cd2a9f42456e4008';
             var imgUrl = `http://image.thum.io/get/auth/${auth}/https://${url}`;
             var pic = $('<img>');
             pic.attr("src", imgUrl);
             $('#img').append(pic);
 
-            var info = $('<div>');
-            info.attr("id", "txt");
-            info.html(`<div class="p-2"><h2>IP: </h2> </div><div class="p-2"><h2>Info: </h2><h2>Child Friendly: </h2> </div><div class="p-2"><h2>Trustworthy: </h2></div>`)
-            $('#stuff').append(info);
-
-            // this is the CORB problem call. URL and authtoken are hardcoded into link for testing purposes
-            // var ipURL = `https://www.enclout.com/api/v1/reputation/show?auth_token=jsvXsM7Cx4AH-r3YURXp&url=google.com`;
-
-            $("#user-link").val("");
-            $('#answer').removeClass("d-none");
-
-            var ipURL = "https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_3fGj57nrEWvCnPmWisKfWJwaVgE7N&domainName=google.com&outputFormat=JSON";
-
-            $.ajax({
-                url: ipURL,
-                method: "GET"
-            }).then(function(response) {
-                console.log(response);
-            });
         }
 
 
@@ -48,10 +53,5 @@ $(document).ready(function() {
 
 
     });
-
-
-    function checkUserLink(string) {
-
-    }
 
 })
